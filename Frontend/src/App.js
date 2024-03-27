385e14fd8d93f2cde5f34df3cd01da8182e7c3d7
LT69018 <jturner3@umbc.edu>
2024-03-26 20:39:11 -0400

    Add notes about pages to implement. Store pckgs..

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,3 +1,22 @@
+// main menu
+// needs buttons for 
+/*
+1. Start/load (1 button, takes you to a terraria like load page).
+- a table of usernames they can select to get back to that id's progress.
+- two buttons on the bottom [back] [new]
+-
+
+2. Choose level
+-> PlayLevel or phaser stuff (tbd)
+-> display the status (win/lose) => they have a button to go back to the menu
+-> take them back to the main menu.
+
+3. View leaderboard
+- a table of stats for each user
+- give them a button to press to go back to the main menu.
+
+*/
+
 import './App.css';
 
 import React, {useState, useEffect} from 'react'
