f2a7c7d0644dab7d3ddf3fafae5575da79bea09a
Connor Gilger <connorgilger@gmail.com>
2024-03-29 16:13:22 -0400

    main menu changes

--- Frontend/src/Pages/MainMenu.js
+++ Frontend/src/Pages/MainMenu.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
+import "react-bootstrap";
 
 import {Container} from 'react-bootstrap';
 import {Typography, Box} from '@mui/material/';
 
 import { useTheme } from '@mui/material/styles';
 
-import 'Frontend\src\CustomButton.css'; // Import CSS file for styling
+import './CustomButton.css'; // Import CSS file for styling
 
 // @cmgilger
-const CustomButton = () => {
+const CustomButton = ({ children }) => {
   return (
-    <button className="custom-button">Click Me</button>
+    <button className="custom-button">{children}</button>
   );
 }
 
@@ -22,15 +23,17 @@ function MainMenu() {
                         backgroundPosition: 'left', 
                         display: 'flex',
                         height: '100vh'}}>
-      <Box bgcolor={theme.palette.background} justifyContent="center">
+      <Row className="justify-content-md-center">
         <Typography variant="h1"
                     color="primary" backgroundColor="secondary">
           Main Menu
-          <CustomButton>Start Game</CustomButton>
-          <CustomButton>Choose Level</CustomButton>
-          <CustomButton>View Leaderboard</CustomButton>
         </Typography>
-      </Box>
+      </Row>
+      <Row>
+        <CustomButton>Start Game</CustomButton>
+        <CustomButton>Choose Level</CustomButton>
+        <CustomButton>View Leaderboard</CustomButton>
+      </Row>
     </Container>
   );
 }
