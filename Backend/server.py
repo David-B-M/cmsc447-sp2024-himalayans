7b0d6452962ce25d271686d173b91ebdc0ea107c
Jessica Turner <124075202+LT69018@users.noreply.github.com>
2024-04-02 17:16:52 -0400

    Create skeleton function w/ SQL for creating users table

--- Backend/server.py
+++ Backend/server.py
@@ -1,11 +1,29 @@
 from flask import Flask
 app = Flask(__name__)
 
+import sqlite3
 
 @app.route('/test')
 def hello_world():  # put application's code here
     return {"sample": ["hello world", "random data", "booo"]}
 
+def create_users_table():
+    """
+    """
+    columns = ["name", "levelReached", ]
+    SQL_FOR_CREATE_USERS = \
+    """
+    CREATE TABLE users (
+        PRIMARY KEY name varchar(5) NOT NULL UNIQUE,
+        levelReached integer NOT NULL,
+    );
+    """
+    # don't let them save a name if its already in the table
+    # the levelReached should always be initialized to 1
+
+    # todo: decide where to store their high scores, and if we want to show them this info when they load.
+    
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
