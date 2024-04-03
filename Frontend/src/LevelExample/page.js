7bcf5e3810d07dda6c2d01a7797ede753e1e1909
ampham03 <apham6@umbc.edu>
2024-04-02 22:07:54 -0400

    Added player movement

--- Frontend/src/LevelExample/page.js
+++ Frontend/src/LevelExample/page.js
@@ -9,60 +9,40 @@ class LevelExampleClass extends Phaser.Scene
 
     preload ()
     {
-        //this.load.atlas('walker', 'assets/animations/walker.png', 'assets/animations/walker.json');
         this.load.image('cat', 'himalayan_cat.jpg');
         this.load.image('background', 'snowy_mountains.jpg');
+        this.load.image('platform', 'platform.jpg');
     }
 
     create ()
     {  
-        const { width, height } = this.sys.game.canvas;
-
         // Create background
-        this.bg = this.add.tileSprite(0, 0, width/2, height/2, 'background').setOrigin(0, 0);
-        this.bg.setScale(width / this.bg.width, height / this.bg.height);
-       
-        /*
-        const animConfig = {
-            key: 'walk',
-            frames: 'walker',
-            frameRate: 60,
-            repeat: -1
-        };
+        const { width, height } = this.sys.game.canvas;
+        //this.bg = this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0, 0);
+        //this.bg.setTileScale(2);
 
-        this.anims.create(animConfig);
-
-        const sprite = this.add.sprite(400, 484, 'walker', 'frame_0000');
-
-        sprite.play('walk');
-        */
+        this.platform = this.add.tileSprite(0, 0, width, height/3, 'platform').setOrigin(0, .5);
+        
+        //this.platform.create(400, 568, 'ground').setScale(2).refreshBody();
 
         this.cursors = this.input.keyboard.createCursorKeys();
 
-        const catSprite = this.add.sprite(400, 484, 'cat');
-        catSprite.setScale(0.1); 
-
-
-        // The player and its settings
-        //this.player = this.physics.add.sprite(100, 450, 'cat');
-        //this.player.setDepth(1);
-/*
-        //  Player physics properties. 
+        this.catSprite = this.physics.add.sprite(200, 484, 'cat');
+        this.catSprite.setScale(0.05); 
         this.catSprite.setBounce(0.2);
         this.catSprite.setCollideWorldBounds(true);
-        this.catSprite.setScale(0.1);
-        */
     
     }
 
     update ()
     {
-        this.bg.tilePositionX += 2;
-/*
-        if (this.cursors.up.isDown && this.catSprite.body.touching.down)
+       //this.bg.tilePositionX += 2;
+       this.platform.tilePositionX += 2;
+
+        if (this.cursors.up.isDown && this.catSprite.body.onFloor())
         {
             this.catSprite.setVelocityY(-330);
-        }*/
+        }
     }
 }
 
@@ -71,12 +51,19 @@ function LevelExample()
 {
     const config = {
         type: Phaser.AUTO,
-        parent: 'phaser-example',
         scale: {
             mode: Phaser.Scale.RESIZE,
             width: '100%',
             height: '100%'
         },
+        physics: {
+            default: 'arcade',
+            arcade: {
+                gravity: { y: 300 },
+                debug: false
+            }
+        },
+        backgroundColor: '#304858',
         scene: LevelExampleClass
     };
 
