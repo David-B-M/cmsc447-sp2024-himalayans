071786b734ca62760256d5c55c8cb85840c52f8c
Jessica Turner <124075202+LT69018@users.noreply.github.com>
2024-04-09 13:41:50 +0000

    Attempt parse request POST /add_user + parse PyTest result. Error 400.
    Clarified in /add_user function header the format to expect upon return (None if failed to add user, otherwise the user_id that corresponds to that row.) Todo: write similar in the README + make sure I'm sure what format I want them to pass the "username" parameter in (right now I'm attempting to accept both json and string parameter via `request.args.get('username')` [see lines 103-107])
    
    Changed method of parsing the request argument because I thought that was the reason I got Error 400 (bad request). Error persists though.
    
    Maybe repl is just unable to run my flask and pytest at the same time so it's actually not getting through to the app?

--- Backend/tests/test_add.py
+++ Backend/tests/test_add.py
@@ -2,7 +2,8 @@ import json
 
 ADD_USER_ENDPOINT = "/add_user"
 
-
+# reference:
+# https://flask.palletsprojects.com/en/3.0.x/testing/#sending-requests-with-the-test-client
 def test_add_valid_username(client):
 
     headers = {
@@ -10,10 +11,13 @@ def test_add_valid_username(client):
     }
     query_dict = {"username": "test1"}
 
-    # reference:
-    # https://flask.palletsprojects.com/en/3.0.x/testing/#sending-requests-with-the-test-client
+    
     response = client.post(ADD_USER_ENDPOINT,
                            query_string=json.dumps(query_dict),
                            headers=headers)
+    parsed_response = response.data.decode('UTF-8')
+    print("*" * 40)
+    print("Got response:", parsed_response)
+    print("*" * 40)
     assert response is not None
     assert response.status_code == 200
