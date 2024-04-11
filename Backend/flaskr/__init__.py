e26ad5fe9432fea61e49576bbe7f480ffc203ebc
samdev193 <101432842+samdev193@users.noreply.github.com>
2024-04-11 17:00:01 -0400

    Added post call to to startGame page and changed "/adduser" route to return data correctly.
    _init_.py:
    -changed /adduser to return json objects with flask.Response()
    -/loadusers still needs to be tested and updated to use flask.Response()
    
    StartGame:
    -added axios post call in start game, it will correctly create users.
    -added function to handle input change for when user enters their name.

--- Backend/flaskr/__init__.py
+++ Backend/flaskr/__init__.py
@@ -3,6 +3,7 @@ import os
 import flask
 import json
 from flask import Flask, request
+import sqlite3
 # hopefully Flask will automatically know to find get_db in db.py considering the Flask documentation recommendaed that structure.
 
 RESPONSE_MESSAGE_KEY = "msg"
@@ -40,7 +41,10 @@ def create_app(test_config=None):
     @app.route('/')
     def home():
         print("Successfully loaded `/` endpoint!")
-        home_response = flask.Response(response= json.dumps({"msg": "Welcome to Everest the Olympicat Backend!"}))
+        response = {
+            RESPONSE_MESSAGE_KEY: "Welcome to Everest the Olympicat Backend!"
+        }
+        home_response = flask.Response(response=json.dumps(response), status=200)
         home_response.headers['Access-Control-Allow-Origin'] = '*'
         home_response.headers["content-type"] = "application/json"
         return home_response
@@ -67,19 +71,24 @@ def create_app(test_config=None):
         RESULT_BOOL_INDEX = 0
         RESULT_USERS_JSON_INDEX = 1
 
+        home_response = flask.Response()
+        home_response.headers['Access-Control-Allow-Origin'] = '*'
+        home_response.headers["content-type"] = "application/json"
+
         loaded_users = db.load_users()
         if not loaded_users[RESULT_BOOL_INDEX]:
             load_response["users"] = []
             add_response_failure_options(load_response,
                                          "Failed to load users.")
-            return load_response
+            home_response.response = json.dumps(load_response)
+            return
 
         # =========
         # Success!
         # =========
         users = loaded_users[RESULT_USERS_JSON_INDEX]
         load_response["users"] = users
-        add_response_success_options(load_response)
+        #add_response_success_options(load_response)
         print("Loaded users: ", users)
         return load_response
 
@@ -109,6 +118,11 @@ def create_app(test_config=None):
         print("%" * 50)
         # get the username they passed to the request.
         username = None
+
+        home_response = flask.Response()
+        home_response.headers['Access-Control-Allow-Origin'] = '*'
+        home_response.headers["content-type"] = "application/json"
+
         if request.headers["Content-Type"] == "application/x-www-form-urlencoded":
             # form data is a multi-dict
             # reference: https://flask-api.github.io/flask-api/api-guide/parsers/
@@ -120,7 +134,9 @@ def create_app(test_config=None):
         if username == None:
             add_response_failure_options(response, 
             "[Endpoint: add_user] Unable to parse username from request. (Got null)")
-            return response
+            home_response.response=json.dumps(response)
+            home_response.status=404
+            return home_response
         # use the database method to try to add the user (validates as well)
         db_add_user_result = db.add_user(username)
 
@@ -128,24 +144,22 @@ def create_app(test_config=None):
             add_response_failure_options(response)
             response[RESPONSE_MESSAGE_KEY] = \
                 f"[Endpoint: add_user] Failed to add user {username} to database. :("
-            return response
+            home_response.response = json.dumps(response)
+            home_response.status = 404
+            return home_response
 
         response["user_id"] = db_add_user_result
-        add_response_success_options(response)
-        return {
-            "ok": True,
-            "msg": f"Successfully added user {username} to database! :D"
-        }
+        response["msg"] = f"Successfully added user {username} to database! :D"
+        home_response.response = json.dumps(response)
+        home_response.status = 200
+        return home_response
 
     # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
-    def add_response_success_options(response):
-        # setting the response headers to be json so we can use things like response.status_code later on hopefully
-        response["headers"] = {"content-type": "application/json"}
-        response["ok"] = True
+
 
     def add_response_failure_options(response,
                                      message="Content-Type not supported!"):
