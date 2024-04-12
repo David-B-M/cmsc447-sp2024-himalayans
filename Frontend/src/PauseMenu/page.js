ce699c8ce8ebf4e6eac80dcce538d3d39fe5f52d
ampham03 <apham6@umbc.edu>
2024-04-12 15:05:10 -0400

    Added text to PauseMenu screen

--- Frontend/src/PauseMenu/page.js
+++ Frontend/src/PauseMenu/page.js
@@ -17,20 +17,21 @@ class PauseScreen extends Phaser.Scene
 
     create ()
     {
+        this.text = this.add.text(530, 100, 'Pause Menu', { font: 'bold 64px Arial' });
 
-        this.resumeLevelBtn = this.add.sprite(450, 110, 'resumeLevelBtn').setOrigin(0, 0);
+        this.resumeLevelBtn = this.add.sprite(450, 160, 'resumeLevelBtn').setOrigin(0, 0);
         this.resumeLevelBtn.setInteractive({ useHandCursor: true });
         this.resumeLevelBtn.setScale(0.5);
 
-        this.resetLevelBtn = this.add.sprite(560, 230, 'resetLevelBtn').setOrigin(0, 0);
+        this.resetLevelBtn = this.add.sprite(560, 280, 'resetLevelBtn').setOrigin(0, 0);
         this.resetLevelBtn.setInteractive({ useHandCursor: true });
         this.resetLevelBtn.setScale(0.5);
 
-        this.quitLevelBtn = this.add.sprite(535, 350, 'quitLevelBtn').setOrigin(0, 0);
+        this.quitLevelBtn = this.add.sprite(535, 400, 'quitLevelBtn').setOrigin(0, 0);
         this.quitLevelBtn.setInteractive({ useHandCursor: true });
         this.quitLevelBtn.setScale(0.5);
 
-        this.backToMainMenuBtn = this.add.sprite(505, 450, 'backToMainMenuBtn').setOrigin(0, 0);
+        this.backToMainMenuBtn = this.add.sprite(505, 500, 'backToMainMenuBtn').setOrigin(0, 0);
         this.backToMainMenuBtn.setInteractive({ useHandCursor: true });
         this.backToMainMenuBtn.setScale(0.5);
 
