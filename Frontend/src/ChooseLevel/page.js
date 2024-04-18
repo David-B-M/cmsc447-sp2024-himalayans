f5214ced9013273401fc389655de1b0775eafb7d
Connor Gilger <connorgilger@gmail.com>
2024-04-18 15:42:40 -0400

    changed getuser

--- Frontend/src/ChooseLevel/page.js
+++ Frontend/src/ChooseLevel/page.js
@@ -4,9 +4,6 @@ import './ChooseLevel.css'; // Import CSS file for styling
 import { useNavigate } from 'react-router-dom';
 import axios from "axios";
 
-let currUser = 1;
-let currLevel = 0;
-
 const BackButton = ({ children }) => {
   const navigate = useNavigate();
 
@@ -26,60 +23,64 @@ const CustomButton = ({ children, to }) => {
   );
 }
 
-//function LevelValue(userID){
-let data = {};
-axios.get('http://127.0.0.1:5000/load_users')
-.then(function (response) {
-  data = response.data;
-  console.log(response);
-  for(let i = 0; i < data["users"].length; i++){
-    //console.log("LevelValue: index = " + i);
-    //console.log("LevelValue: id = " + data["users"][i]['user_id']);
-    //console.log("LevelValue: currUser = " + currUser)
-    if(Number(currUser) == Number(data["users"][i]['user_id'])){
+function ChooseLevel() {
+  let currUser = 1;
+  let currLevel = 0;
+  const [data, setData] = useState([{}])
+  const [name, setName] = useState("")
+  const loadUser = () => {
+        axios.get("http://localhost:5000/load_users").then(res => {
+          setData(res.data)
+          console.log(data)
+      }
+  ).catch(e => {
+      console.log(e);
+      })
+  }
+  useEffect(() => {
+      loadUser()
+  }, []);
+  //}
+  //let currLevel = LevelValue(currUser);
+
+  for(let i = 0; i < res.data["users"].length; i++){
+    if(Number(currUser) == Number(res.data["users"][i]['user_id'])){
       //console.log("levelReached = " + data["users"][i]['levelReached'])
       let level = data["users"][i]['levelReached'];
       console.log("LevelValue: Level returned: " + level);
       currLevel = level;
     }
-  }
-})
-.catch(function (error) {
-  console.log(error);
-});
-//}
+  };
 
-const ButtonSwitch = () => { // This determines how the buttons are displayed.
-  let buttons;
-  //let currLevel = level;
-  console.log("ButtonSwitch: level = " + Number(currLevel));
-  //switch(Number(currLevel)){
-  if(Number(currLevel) == 1){ //Level 1 is currLevel
-    buttons = <div><CustomButton to="/LevelExample">Level 1</CustomButton>
-    <CustomButton disabled>[LOCKED]</CustomButton>
-    <CustomButton disabled>[LOCKED]</CustomButton></div>
+  const ButtonSwitch = () => { // This determines how the buttons are displayed.
+    let buttons;
+    //let currLevel = level;
+    console.log("ButtonSwitch: level = " + Number(currLevel));
+    //switch(Number(currLevel)){
+    if(Number(currLevel) == 1){ //Level 1 is currLevel
+      buttons = <div><CustomButton to="/LevelExample">Level 1</CustomButton>
+      <CustomButton disabled>[LOCKED]</CustomButton>
+      <CustomButton disabled>[LOCKED]</CustomButton></div>
+    }
+    if(Number(currLevel) == 2){ //Level 2 is currLevel
+      buttons = <div><CustomButton to="/LevelExample">Level 1</CustomButton>
+      <CustomButton>Level 2</CustomButton>
+      <CustomButton disabled>[LOCKED]</CustomButton></div>
+      //break;
+    }
+    if(Number(currLevel) == 3){ //Level 3 is currLevel
+      buttons = <div><CustomButton to="/LevelExample">Level 1</CustomButton>
+      <CustomButton>Level 2</CustomButton>
+      <CustomButton>Level 3</CustomButton></div>
+      //break;
+    }
+    else{
+      buttons = <div><CustomButton disabled>Please Load Game and Return!</CustomButton></div>
+    }
+    //}
+    return <div>{buttons}</div>
   }
-  if(Number(currLevel) == 2){ //Level 2 is currLevel
-    buttons = <div><CustomButton to="/LevelExample">Level 1</CustomButton>
-    <CustomButton>Level 2</CustomButton>
-    <CustomButton disabled>[LOCKED]</CustomButton></div>
-    //break;
-  }
-  if(Number(currLevel) == 3){ //Level 3 is currLevel
-    buttons = <div><CustomButton to="/LevelExample">Level 1</CustomButton>
-    <CustomButton>Level 2</CustomButton>
-    <CustomButton>Level 3</CustomButton></div>
-    //break;
-  }
-  else{
-    buttons = <div><CustomButton disabled>Please Load Game and Return!</CustomButton></div>
-  }
-  //}
-  return <div>{buttons}</div>
-}
 
-function ChooseLevel() {
-  //let currLevel = LevelValue(currUser);
   console.log("ChooseLevel: LevelValue = " + currLevel);
   return (
     <div style={{ backgroundImage: `url('snowy_mountains.jpg')`,  
