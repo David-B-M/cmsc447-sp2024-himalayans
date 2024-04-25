9af417600107b5d8c9cd7dd298c6a3b64b820a11
samdev193 <101432842+samdev193@users.noreply.github.com>
2024-04-25 16:40:55 -0400

    Adjusted level 2 to be able to be completed without powerups.

--- Frontend/src/LevelTwo/page.js
+++ Frontend/src/LevelTwo/page.js
@@ -276,7 +276,7 @@ class LevelTwoClass extends Phaser.Scene
             this.shieldTimeLeft -= 0.025;
         }
 
-        if (Math.abs(this.timerValue % 15) < 0.025) {
+        if (Math.abs(this.timerValue % 8) < 0.025) {
             spawnBoulder(this);
         }
 
@@ -302,10 +302,10 @@ function spawnTree(scene)
 }
 function spawnBoulder(scene)
 {
-    const boulder = scene.boulders.create(1000, 300, 'boulder')
+    const boulder = scene.boulders.create(700, 300, 'boulder')
             .setAccelerationX(-100)
             .setBounce(.5)
-            .setScale(.45);
+            .setScale(.40);
 
         scene.physics.world.on('worldstep', () =>
         {
@@ -316,7 +316,7 @@ function spawnBoulder(scene)
 }
 function spawnPowerup(scene)
 {
-    const powerup = Math.floor(Math.random() * 4) + 1;;
+    const powerup = Math.floor(Math.random() * 4) + 1;
     if (powerup === 1)
     {
         const jumpBoost = scene.jumpBoosts.create(1400, getRandomY(), 'jumpBoost');
