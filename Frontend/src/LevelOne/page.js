e8de1c8d2906609de93df7f36b44b562b6f738ac
ampham03 <apham6@umbc.edu>
2024-04-25 17:06:56 -0400

    Added additional sound effects

--- Frontend/src/LevelOne/page.js
+++ Frontend/src/LevelOne/page.js
@@ -31,6 +31,10 @@ class LevelOneClass extends Phaser.Scene
         this.load.audio('collect', 'collect.mp3');
         this.load.audio('jump', 'jump.mp3');
         this.load.audio('gameOver', 'game_over.mp3');
+        this.load.audio('pickUpJumpBoost', 'Yippee.wav');
+        this.load.audio('pickUpShield', 'shield.mp3');
+        this.load.audio('pickUpSpeedBoost', 'speed.mp3');
+        this.load.audio('pickUpClock', 'clock.mp3');
     }
 
     create() {
@@ -326,12 +330,14 @@ function spawnPowerup(scene)
 
 function collectClock(player, clock)
 {
+    this.sound.play('pickUpClock');
     clock.disableBody(true, true);
     this.timerValue += 5;
 }
 
 function collectShield(player, shield)
 {
+    this.sound.play('pickUpShield');
     shield.disableBody(true, true);
     this.shieldActive = true;
     this.shieldTimeLeft = powerUpTime;
@@ -339,6 +345,7 @@ function collectShield(player, shield)
 
 function collectSpeedBoost(player, speedBoost)
 {
+    this.sound.play('pickUpSpeedBoost');
     speedBoost.disableBody(true, true);
     this.speedBoostActive = true;
     this.speedBoostTimeLeft = powerUpTime;
@@ -346,6 +353,7 @@ function collectSpeedBoost(player, speedBoost)
 
 function collectJumpBoost(player, jumpBoost)
 {
+    this.sound.play('pickUpJumpBoost');
     jumpBoost.disableBody(true, true);
     this.jumpBoostActive = true;
     this.jumpBoostTimeLeft = powerUpTime;
--- Frontend/src/LevelTwoPauseMenu/page.js
+++ Frontend/src/LevelTwoPauseMenu/page.js
@@ -22,18 +22,22 @@ class LevelTwoPauseMenu extends Phaser.Scene
         this.resumeLevelBtn = this.add.sprite(450, 160, 'resumeLevelBtn').setOrigin(0, 0);
         this.resumeLevelBtn.setInteractive({ useHandCursor: true });
         this.resumeLevelBtn.setScale(0.5);
+        this.resumeLevelBtn.tint = 0xFF9999;
 
         this.resetLevelBtn = this.add.sprite(560, 280, 'resetLevelBtn').setOrigin(0, 0);
         this.resetLevelBtn.setInteractive({ useHandCursor: true });
         this.resetLevelBtn.setScale(0.5);
+        this.resetLevelBtn.tint = 0xFF9999;
 
         this.quitLevelBtn = this.add.sprite(535, 400, 'quitLevelBtn').setOrigin(0, 0);
         this.quitLevelBtn.setInteractive({ useHandCursor: true });
         this.quitLevelBtn.setScale(0.5);
+        this.quitLevelBtn.tint = 0xFF9999;
 
         this.backToMainMenuBtn = this.add.sprite(505, 500, 'backToMainMenuBtn').setOrigin(0, 0);
         this.backToMainMenuBtn.setInteractive({ useHandCursor: true });
         this.backToMainMenuBtn.setScale(0.5);
+        this.backToMainMenuBtn.tint = 0xFF9999;
 
         this.resumeLevelBtn.on('pointerdown', () =>
         {
